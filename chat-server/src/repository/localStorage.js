a55218494754c4a8b123cdeecdf63a41c22463d4
Tiago Figueiredo de Carvalho <tiago.figcarvalho@gmail.com>
2020-04-22 00:25:50 -0300

    Ajuste na configuração do local Storage

--- chat-server/src/repository/localStorage.js
+++ chat-server/src/repository/localStorage.js
@@ -1,6 +1,6 @@
 if (typeof localStorage === "undefined" || localStorage === null) {
     var LocalStorage = require('node-localstorage').LocalStorage;
-    localStorage = new LocalStorage('../data/chat');
+    localStorage = new LocalStorage('./data/chat');
 }
 
 module.exports = {localStorage};
